788a7f1ef06ded71e924c91a431841b9874739c6
tomas <tomascostapp@gmail.com>
2025-10-17 18:51:23 -0300

    D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,10 +29,10 @@ export default function Home() {
           <CalificationFormDirect variant={variant} />
         )
       }
-      <header className="bg-[#fbff00] max-w-[85%] w-[400px] rounded-full mt-8 md:mt-12 mx-auto">
+      <header className="bg-[#fbff00] max-w-[85%] w-[500px] rounded-full mt-8 md:mt-12 mx-auto">
         <div className="cf-container">
           <h3
-            className="text-center text-black text-[12px] py-3 font-bold leading-[115%]"
+            className="text-center text-black text-[14px] py-3 font-bold leading-[115%]"
           >
             Te exigis, te castigas, lo das todo… y aún así ¿no te reconoces frente al espejo?
           </h3>
