e77973a5f1d000f80f972dab266606f5ffd2225b
tomas <tomascostapp@gmail.com>
2025-11-24 20:05:32 -0300

    d

--- src/app/pages/nothing-for-you-now/page.tsx
+++ src/app/pages/nothing-for-you-now/page.tsx
@@ -1,7 +1,7 @@
 export default function Unqualified() {
 	return (
 		<>
-			<section className="py-[100px]">
+			<section className="py-[100px] px-4">
 				<h1 className="text-center max-w-[800px] mx-auto text-[32px] leading-[110%] font-bold">Por el momento no tenemos un servicio acorde a lo que marcaste</h1>
 			</section>
 		</>
